ca9f97faeacd2b85a4ec9067fee668ce17d55917
temio <temioyelade8@gmail.com>
2023-01-24 15:21:13 +0000

    created local storage data

--- starter/assets/script.js
+++ starter/assets/script.js
@@ -25,6 +25,14 @@ $(".saveBtn").click(function (event) {
         localStorage.setItem(time,value);
     });
 
-
+$("#9am .task").val(localStorage.getItem("09"));
+    $("#10am .task").val(localStorage.getItem("10"));
+    $("#11am .task").val(localStorage.getItem("11"));
+    $("#12pm .task").val(localStorage.getItem("12"));
+    $("#1pm .task").val(localStorage.getItem("13"));
+    $("#2pm .task").val(localStorage.getItem("14"));
+    $("#3pm .task").val(localStorage.getItem("15"));
+    $("#4pm .task").val(localStorage.getItem("16"));
+    $("#5pm .task").val(localStorage.getItem("17"));
     
    
