713d11b8731386590a818538a1325356d5a1b7b3
temio <temioyelade8@gmail.com>
2023-01-24 15:17:02 +0000

    created past present and future functionality

--- starter/assets/script.js
+++ starter/assets/script.js
@@ -1,3 +1,22 @@
 var today = moment()
 $("#currentDay").text(today.format("dddd MMMM Do"));
 
+$(".taskRow").each(function() {
+        var taskRow = $(this).attr("id").split("-")[1];
+  
+     if (currentHour == taskRow) {
+            $(this).addClass("present");
+            $(this).children(".description").addClass("present");   
+    
+     } else if (currentHour < taskRow) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+
+        } else if (currentHour > taskRow) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+});
+
+
+   
